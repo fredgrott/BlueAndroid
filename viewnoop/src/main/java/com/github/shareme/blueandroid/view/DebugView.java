9f92a57c775401efe8164d55f1a9c3c0d3af4229
Fred Grott <fred.grott@gmail.com>
2016-10-14 18:03:04 -0500

    added viewnoop module code

---
+++ viewnoop/src/main/java/com/github/shareme/blueandroid/view/DebugView.java
@@ -0,0 +1,58 @@
+package com.github.shareme.blueandroid.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import com.github.shareme.blueandroid.base.DebugModule;
+
+@SuppressWarnings("unused")
+public class DebugView extends LinearLayout {
+
+    public DebugView(Context context) {
+        super(context);
+        setOrientation(VERTICAL);
+    }
+
+    public DebugView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setOrientation(VERTICAL);
+    }
+
+    public DebugView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setOrientation(VERTICAL);
+    }
+
+    /**
+     * Calls modules {@link DebugModule#onResume()} method
+     */
+    public void onResume() {
+
+    }
+
+    /**
+     * Calls modules {@link DebugModule#onPause()} method
+     */
+    public void onPause() {
+
+    }
+
+    /**
+     * Starts all modules and calls their {@link DebugModule#onStart()} method
+     */
+    public void onStart() {
+
+    }
+
+    /**
+     * Removes all modules and calls their {@link DebugModule#onStop()} method
+     */
+    public void onStop() {
+
+    }
+
+    public void modules(DebugModule... drawerItems) {
+
+    }
+}
