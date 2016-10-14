d5daa2ae43a9804e329d68d7b49800f765506d9d
Fred Grott <fred.grott@gmail.com>
2016-10-14 18:36:22 -0500

    added actions module code

---
+++ actions/src/main/java/com/github/shareme/blueandroid/actions/ActionsModule.java
@@ -0,0 +1,88 @@
+package com.github.shareme.blueandroid.actions;
+
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.github.shareme.blueandroid.base.DebugModule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@SuppressWarnings("unused")
+public class ActionsModule implements DebugModule {
+    private final List<Action> actions = new ArrayList<>();
+
+    public ActionsModule(Action... actions) {
+        if (actions != null) {
+            for (int i = 0; i < actions.length; i++) {
+                this.actions.add(actions[i]);
+            }
+        }
+    }
+
+    @NonNull @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup parent) {
+        LinearLayout view = (LinearLayout) inflater.inflate(R.layout.debug_drawer_module_actions, parent, false);
+
+        if (actions.isEmpty()) {
+            TextView noActionsLabel = new TextView(parent.getContext());
+            noActionsLabel.setTextColor(ContextCompat.getColor(parent.getContext(), R.color.white));
+            noActionsLabel.setText("No actions added");
+            view.addView(noActionsLabel);
+        } else {
+            for (Action action : actions) {
+                view.addView(action.getView(view));
+            }
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onOpened() {
+        for (Action action : actions) {
+            action.onOpened();
+        }
+    }
+
+    @Override
+    public void onClosed() {
+        for (Action action : actions) {
+            action.onClosed();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        for (Action action : actions) {
+            action.onResume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        for (Action action : actions) {
+            action.onPause();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        for (Action action : actions) {
+            action.onStart();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        for (Action action : actions) {
+            action.onStop();
+        }
+    }
+}
